ea5a7c2768618b6de15ec01ad1d2b9eae7a852e7
samuelcardoso <samuelparacontato@gmail.com>
2019-12-27 00:31:20 -0300

    setting references between servers

--- back/src/websocket/main.ts
+++ back/src/websocket/main.ts
@@ -1,10 +1,17 @@
 const WebSocketClient = require('ws');
 const ws = new WebSocketClient('ws://sim:8080/quotes');
+const redis = require("redis"),
+    redisClient = redis.createClient({host: 'redis'});
+
+redisClient.on("error", (err) => {
+    console.error(err);
+});
 
 ws.on('open', () => {
-    console.log('OK!');
+    console.debug('Connected');
 });
 
 ws.on('message', (data) => {
-    console.log(data);
+    const json = JSON.parse(data);
+    redisClient.set(Object.keys(json)[0], JSON.stringify(json));
 });
